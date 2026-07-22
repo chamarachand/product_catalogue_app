f9ce563cf62a4665312d45a9714abcd4a0f02d3e
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2026-07-22 13:19:25 +0530

    Add Api details

---
+++ lib/core/constants/url_constants.dart
@@ -0,0 +1,5 @@
+abstract class UrlConstants {
+  static const String baseUrl = 'https://dummyjson.com';
+  static const String products = '/products';
+  static String productById(int id) => '/products/$id';
+}
